fc146d018d6f4e2235c9cfe5c95d4c3a0c68dc34
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2025-04-14 22:50:46 +0530

    FastAPI Assignment Project1

--- course/FastAPI_Course/Project1-Books/main.py
+++ course/FastAPI_Course/Project1-Books/main.py
@@ -35,6 +35,21 @@ async def read_category_by_query(category: str):
 
     return books_to_return
 
+
+'''
+Get all books from a specific author using path or query parameters
+'''
+
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
 @app.get("/books/{book_author}/")
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
@@ -64,3 +79,4 @@ async def delete_book(book_title: str):
             BOOKS.pop(i)
             break
     
+
