5c54bd1ad343d704779b19aebfada35e59a07354
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2025-04-14 22:02:04 +0530

    FastAPI Project: Query Parameters

--- course/FastAPI_Course/Project1-Books/main.py
+++ course/FastAPI_Course/Project1-Books/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-Books = [
+BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
@@ -16,12 +16,31 @@ Books = [
 
 @app.get('/books')
 async def read_all_books():
-    return Books
+    return BOOKS
 
 
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
-    for book in Books:
+    for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+        book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
