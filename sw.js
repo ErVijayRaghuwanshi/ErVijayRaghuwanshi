e05ea5f9aa75eb03588041b5a8d471d5047f6e06
Vijay Raghuwanshi <ervijayraghuwanshi@gmail.com>
2025-08-02 23:17:13 +0530

    adding missing assets for pwa

--- sw.js
+++ sw.js
@@ -2,8 +2,20 @@ const CACHE_NAME = "simple-pwa-cache-v1";
 const urlsToCache = [
   "ErVijayRaghuwanshi/index.html", 
   "ErVijayRaghuwanshi/manifest.json", 
+  "ErVijayRaghuwanshi/sw.js",
   "ErVijayRaghuwanshi/script.js", 
-  "ErVijayRaghuwanshi/style.css"
+  "ErVijayRaghuwanshi/style.css",
+  "ErVijayRaghuwanshi/assets/profile.jpeg",
+  "ErVijayRaghuwanshi/assets/Vijay_Raghuwanshi_Resume.pdf",
+  "ErVijayRaghuwanshi/assets/screenshots/desktop.png",
+  "ErVijayRaghuwanshi/assets/icon-32x32.png", 
+  "ErVijayRaghuwanshi/assets/icon-48x48.png",
+  "ErVijayRaghuwanshi/assets/icon-72x72.png",
+  "ErVijayRaghuwanshi/assets/icon-96x96.png", 
+  "ErVijayRaghuwanshi/assets/icon-144x144.png",
+  "ErVijayRaghuwanshi/assets/icon-152x152.png",
+  "ErVijayRaghuwanshi/assets/icon-128x128.png",
+  "ErVijayRaghuwanshi/assets/icon-192x192.png",
 ];
 
 self.addEventListener("install", event => {
